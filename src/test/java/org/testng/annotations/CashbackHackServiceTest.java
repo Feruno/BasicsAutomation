2bc243fea396d76d5be9cd74cdd3d15838180093
Александр Бондаренко <nice.alexsandr@mail.ru>
2023-07-29 14:22:50 +0300

    testng n2

--- src/test/java/org/testng/annotations/CashbackHackServiceTest.java
+++ src/test/java/org/testng/annotations/CashbackHackServiceTest.java
@@ -7,13 +7,23 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
 
     @org.testng.annotations.Test
-    public void testRemain() {
+    public void testRemainNg() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
 
         int actual = service.remain(amount);
         int expected = 100;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainZeroNg() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
     }
 
 }
